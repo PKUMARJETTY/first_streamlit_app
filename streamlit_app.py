0ee36c6ded6522e593a226725c1a12ad23219132
PKUMARJETTY <127284489+PKUMARJETTY@users.noreply.github.com>
2023-03-08 12:24:31 +0530

    Update streamlit_app.py
    Trying the emo images

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,13 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+#Trying the EMO images
+# grinning face
+print("\U0001f600")
+ 
+# grinning squinting face
+print("\U0001F606")
+ 
+# rolling on the floor laughing
+print("\U0001F923")
